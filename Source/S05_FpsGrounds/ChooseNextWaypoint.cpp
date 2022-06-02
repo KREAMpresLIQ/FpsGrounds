365198047dc41892f2f61e980a2f7b6b6b585464
csajkasnorbert <csajkasnorbert8637@gmail.com>
2022-06-02 17:19:09 +0200

    Blackboard adat beolvasas (Data Read)

--- Source/S05_FpsGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_FpsGrounds/ChooseNextWaypoint.cpp
@@ -2,9 +2,13 @@
 
 #include "S05_FpsGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++!!"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"));
+	
 	return EBTNodeResult::Succeeded;
 }
--- Source/S05_FpsGrounds/ChooseNextWaypoint.h
+++ Source/S05_FpsGrounds/ChooseNextWaypoint.h
@@ -15,5 +15,9 @@ class S05_FPSGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 	
 };
