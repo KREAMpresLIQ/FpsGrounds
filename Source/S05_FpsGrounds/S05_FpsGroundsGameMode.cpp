b519d6187968635571a740de39ea980e87915d6e
csajkasnorbert <csajkasnorbert8637@gmail.com>
2022-06-14 19:59:12 +0200

    Update, error fix

--- Source/S05_FpsGrounds/S05_FpsGroundsGameMode.cpp
+++ Source/S05_FpsGrounds/S05_FpsGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05_FpsGroundsGameMode::AS05_FpsGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
