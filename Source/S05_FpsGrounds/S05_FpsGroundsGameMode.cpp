df25f0eb1c98e3fd2a47bf6f43cc74deaf84e82f
csajkasnorbert <csajkasnorbert8637@gmail.com>
2022-06-24 13:42:40 +0200

    TP Merge | Refactoring

--- Source/S05_FpsGrounds/S05_FpsGroundsGameMode.cpp
+++ Source/S05_FpsGrounds/S05_FpsGroundsGameMode.cpp
@@ -9,8 +9,10 @@ AS05_FpsGroundsGameMode::AS05_FpsGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	//Blueprint'/Game/Dynamic/Character/Behaviour/Character.Character'
 
 	// use our custom HUD class
 	HUDClass = AS05_FpsGroundsHUD::StaticClass();
